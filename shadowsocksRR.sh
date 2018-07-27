50f839a3c9d41f8577d4e81de632455578e64e75
Molly Lau <windowsbeta@outlook.com>
2018-07-27 19:28:54 +0800

    Update shadowsocksRR.sh

--- shadowsocksRR.sh
+++ shadowsocksRR.sh
@@ -188,9 +188,12 @@ pre_install(){
         exit 1
     fi
     # 设置 ShadowsocksRR 密码
-    echo "Please input password for ShadowsocksRR:"
-    read -p "(Default password: MollyLau):" shadowsockspwd
-    [ -z "${shadowsockspwd}" ] && shadowsockspwd="MollyLau"
+    while true
+    do
+    dpwd=$(uuidgen) # 调取系统内置的uuidgen组件，将当前读取到的UUID，存储在dpwd变量中
+    echo "Please input password for ShadowsocksRR(Default will generate a new UUID):"
+    read -p "(Default password: ${dpwd}):" shadowsockspwd
+    [ -z "${shadowsockspwd}" ] && shadowsockspwd=${dpwd}
     echo
     echo "---------------------------"
     echo "password = ${shadowsockspwd}"
