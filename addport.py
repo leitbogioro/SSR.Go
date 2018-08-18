b2f7492dc20bb206dca8fd0958a6ac2d3ead8523
Molly Lau <windowsbeta@outlook.com>
2018-08-19 04:02:47 +0800

    Update addport.py

--- addport.py
+++ addport.py
@@ -3,10 +3,22 @@
 
 import os
 import readjson
+import subprocess
 
 CurPort=str(readjson.ConfPort)
+TcpPort = "iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport " + CurPort +" -j ACCEPT"
+UdpPort = "iptables -I INPUT -m state --state NEW -m udp -p udp --dport " + CurPort +" -j ACCEPT"
 
-AddTcpPort = "iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport " + CurPort +" -j ACCEPT"
-AddUdpPort = "iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport " + CurPort +" -j ACCEPT"
-os.system(AddTcpPort)
-os.system(AddUdpPort)
+# 决定是否增加端口
+def JudgeAddPort():
+    # 设置查询 iptables 规则
+    SetRules='iptables -L INPUT -n --line-numbers | grep ' + CurPort
+    # 调用 shell
+    runShell=subprocess.Popen(SetRules, shell=True, stdout=subprocess.PIPE)
+    # 输出包含 SSR 端口的防火墙规则
+    shellOutput=runShell.communicate()[0]
+    if int(shellOutput.find(CurPort)) == 0:
+        os.system(TcpPort)
+        os.system(UdpPort)
+
+JudgeAddPort()
