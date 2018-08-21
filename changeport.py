d22085746fa6cf8db56839fbfd59b230150ce987
Molly Lau <windowsbeta@outlook.com>
2018-08-21 21:07:06 +0800

    Update changeport.py

--- changeport.py
+++ changeport.py
@@ -8,7 +8,7 @@ from ssrextra import is_number
 # 主程序
 
 print ("Current port is: %s") % str(readjson.ConfPort)
-print ("Would you like to generate a new random port (r) or custom a new port by yourself (m)? ")
+print ("Would you like to generate a new random port (r) or custom it by yourself (m)? ")
 ifgenport = raw_input()
 
 if ifgenport == "r" or ifgenport == '':
@@ -16,12 +16,12 @@ if ifgenport == "r" or ifgenport == '':
     writejson.WritePort(newport)
     print("New port is: %s") % newport
 elif ifgenport == "m":
-    print ("Please input a new port: ")
+    print ("Please pick a new port: ")
     newport = raw_input()
     if (is_number(newport)) and int(newport) >= 0 and int(newport) <= 65535:
 	writejson.WritePort(newport)
         print("New port is: %s") % newport
     else:
-	print("Input error, please input a number between 1 and 65535 ! ")
+	print("Input error, please pick a number between 1 and 65535 ! ")
 else:
-    print("Input error, please input 'r' or 'm'! ")
+    print("Incorrect input, please press 'r' or 'm'! ")
