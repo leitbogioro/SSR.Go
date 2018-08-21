d74b8b2d2f9825f1e092737865a3d5876578b03f
Molly Lau <windowsbeta@outlook.com>
2018-08-21 20:27:30 +0800

    Update changepwd.py

--- changepwd.py
+++ changepwd.py
@@ -4,24 +4,24 @@ import ssrextra
 import readjson
 import writejson
 
-print ("当前密码为：%s") % str(readjson.ConfPwd)
-print ("是否要随机生成一个新的密码 (y/n)：")
+print ("Current password is : %s") % str(readjson.ConfPwd)
+print ("Need to generate a new password? (y/n)：")
 ifgenpwd = raw_input()
 def genradompwd(length):
     newpwd=ssrextra.Pwd(length)
     writejson.WritePwd(newpwd)
-    print("新的密码为：%s") % newpwd
+    print("New password is: %s") % newpwd
 
 if ifgenpwd == "y" or ifgenpwd == '':
     genradompwd(16)
 elif ifgenpwd=="n":
-    print("请输入新密码")
+    print("Please input a new password: ")
     newpwd = raw_input()
     if newpwd == "":
-	print ("密码不可为空")
+	print ("An empty password is not allowed! ")
 	genradompwd(16)
     else:
 	writejson.WritePwd(newpwd)
-	print("新的密码为：%s") % newpwd
+	print("New password is: %s") % newpwd
 else:
-    print("输入不正确，请输入 y 或 n")
+    print("Input incorrect! Please input y or n! ")
