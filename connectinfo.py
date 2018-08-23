6e32378b3b7a2d7e854187eaf692469b052b4bc5
Molly Lau <windowsbeta@outlook.com>
2018-08-23 17:36:10 +0800

    Update connectinfo.py

--- connectinfo.py
+++ connectinfo.py
@@ -14,7 +14,7 @@ import socket
 def get_host_ip():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
+        s.connect(('8.8.8.8', 53))
         ip = s.getsockname()[0]
     finally:
         s.close()
