f6a2f475bdc85033b3aa7afbbf72a9c843e467ef
Molly Lau <windowsbeta@outlook.com>
2018-08-24 20:40:11 +0800

    Update ssrinstallEn.sh

--- ssrinstallEn.sh
+++ ssrinstallEn.sh
@@ -107,7 +107,7 @@ centosversion(){
 # 获得公共 IP 地址
 get_ip(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
-    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 api.ipify.org )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
     [ ! -z ${IP} ] && echo ${IP} || echo
 }
