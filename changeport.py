8f7245a0fb8fe873a799273f3bdc8beb9f9eddaa
Molly Lau <windowsbeta@outlook.com>
2018-09-05 14:59:57 +0800

    Update changeport.py

--- changeport.py
+++ changeport.py
@@ -1,27 +1,32 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import ssrextra
-import readjson
+import re
+from readjson import ConfPort
 import writejson
-from ssrextra import is_number
+from ssrextra import (is_number, Port, Show_conf)
 
 # 主程序
-
-print ("当前主端口为：%s") % str(readjson.ConfPort)
+Show_conf("当前端口", ConfPort)
 print ("生成随机端口 (r) 还是自定义端口 (m)：")
 ifgenport = raw_input()
+functype = re.search('[RrMm]', ifgenport)
 
-if ifgenport == "r" or ifgenport == '':
-    newport=ssrextra.Port(10000, 65535)
+while not functype:
+    print ("输入不正确，请输入 r 或 m")
+    print ("生成随机端口 (r) 还是自定义端口 (m)：")
+    ifgenport = raw_input()
+    functype = re.search('[RrMm]', ifgenport)
+if ifgenport == "r" or ifgenport == "R":
+    newport=Port(10000, 65535)
     writejson.WritePort(newport)
-    print("新的端口为：%s") % newport
-elif ifgenport == "m":
+    Show_conf("新的端口", newport)
+else:
     print ("请输入新端口：")
     newport = raw_input()
-    if (is_number(newport)) and int(newport) >= 0 and int(newport) <= 65535:
-	writejson.WritePort(newport)
-        print("新的端口为：%s") % newport
-    else:
-	print("输入错误，请输入 1 至 65535 之间的数字！")
-else:
-    print("输入不正确，请输入 r 或 m")
+    while not (is_number(newport)) or int(newport) < 0 or int(newport) > 65535:
+        print ("输入错误，请输入 1 至 65535 之间的数字！")
+        print ("")
+        print ("请输入新端口：")
+        newport = raw_input()
+    writejson.WritePort(newport)
+    Show_conf("新的端口", newport)
