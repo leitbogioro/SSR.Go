d61ff49fd1db0eb3f34cca0d8fa39db37581e976
Molly Lau <windowsbeta@outlook.com>
2018-12-15 11:11:41 +0800

    添加选择界面

--- setredirect.py
+++ setredirect.py
@@ -4,4 +4,13 @@ from readjson import ConfRedirect
 import writejson
 from ssrextra import Show_conf
 
+print ("须知：设置重定向，意味着当来访请求不符合 ShadowSocksR 数据包类型的时候，ShadowSocksR 会自动将该请求
+重定向到另一个本机的或其他域的空间。")
 Show_conf("当前重定向参数", ConfRedirect)
+print ("请选择你想执行的操作：")
+print ("")
+print ("0.返回上一级")
+print ("1.指定访问本地的某端口重定向到哪个网站")
+print ("2.一键将访问本地的80、443端口重定向到某网站")
+print ("3.一键将访问本地的80、443端口重定向到本机某端口")
+newredirect = raw_input()
